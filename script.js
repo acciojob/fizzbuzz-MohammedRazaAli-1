7cda2812f0f62156de417da0cc66775d6082b6a1
MohammedRazaAli-1 <144483340+MohammedRazaAli-1@users.noreply.github.com>
2025-01-06 11:24:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let output="";
+for(let i=1; i<=20; i++){
+	if(i%3==0 && i%5==0){
+		output="FizzBuzz";
+		
+	}else if(i%5==0){
+		output="Buzz";
+		
+	}else if(i%3==0){
+		output="Fizz";
+	}else{
+		output = i.toString();
+	}
+}
+alert(output);
